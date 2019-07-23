4dd974676a7c70421614dce1774574914323a96c
StefanSim1 <46597302+StefanSim1@users.noreply.github.com>
2019-07-23 11:59:19 +0300

    fixed bugs
    -when the player shoots he does not kill himself anymore.
    -projectiles are not going through asteroids
    -layer order

---
+++ s193800_unity_assignment/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] float health = 500;
+    public float maxThrust;
+    public float maxTorque;
+    public Rigidbody2D rb;
+   
+    void Start()
+    {
+        Vector2 thrust = new Vector2(Random.Range(-maxThrust, maxThrust), Random.Range(-maxThrust, maxThrust));
+        float torque = Random.Range(-maxTorque, maxTorque);
+
+        rb.AddForce(thrust);
+        rb.AddTorque(torque);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        HitProcess(damageDealer);
+    }
+
+    private void HitProcess(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+    
